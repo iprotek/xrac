95ca0c3793074ac63d99f0cbd6ada0d0ca0e5791
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-06-06 12:37:09 +0800

    Update policy-control.php

--- routes/api/policy-control.php
+++ routes/api/policy-control.php
@@ -5,8 +5,31 @@ use iProtek\PolicyControl\Http\Controllers\PolicyControlController;
 
 
 Route::prefix('policy-control')->name('.policy-control')->group(function(){
+
     Route::get('list', [PolicyControlController::class, 'list'])->name('.list')
         ->defaults("_description", "List of policy control")
         ->defaults("_is_visible", true)
         ->defaults("_is_allow", false);
+
+    Route::post('update-role', [PolicyControlController::class, 'update_role'])->name('.update-role')
+        ->defaults("_description", "Update role access to policy control")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false);
+
+    Route::post('update-user-disable-routes', [PolicyControlController::class, 'update_user_disable_routes'])->name('.update-user-disable-routes')
+        ->defaults("_description", "Update user disable routes access to policy control")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false);
+
+    Route::get('role-routes/{xrole_id}', [PolicyControlController::class, 'get_role_routes'])->name('.get-role-routes')
+        ->defaults("_description", "Get role access to policy control")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false);
+
+    Route::get('user-disable-routes/{app_account_id}', [PolicyControlController::class, 'get_user_disable_routes'])->name('.get-user-disable-routes')
+        ->defaults("_description", "Get user disable routes access to policy control")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false);
+
+
 });
